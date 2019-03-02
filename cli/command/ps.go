4806868c8194aecb9d37fc4275fd1e4b8405c243
songxinjianqwe <songxinjianzx@163.com>
2019-03-02 23:30:43 +0800

    尝试实现ps命令，需要先实现exec命令。 目前发现kill命令有问题，信号发出去但是进程仍在运行。

--- cli/command/ps.go
+++ cli/command/ps.go
@@ -2,10 +2,14 @@ package command
 
 import "github.com/urfave/cli"
 
+/**
+相当于exec ps
+*/
 var PsCommand = cli.Command{
 	Name:  "ps",
-	Usage: "show a container's info",
+	Usage: "show a container's process info",
 	Action: func(ctx *cli.Context) error {
+
 		return nil
 	},
 }
