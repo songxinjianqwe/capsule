9a05dab092273b39dac2b1d4f6a42b1c2e556765
songxinjianqwe <songxinjianzx@163.com>
2019-03-07 10:13:17 +0800

    修复了在exec时也带了cloneflags导致setns失败。 但是又出现这个问题：runtime/cgo: pthread_create failed: Invalid argument

--- libcapsule/nsenter/nsenter.go
+++ libcapsule/nsenter/nsenter.go
@@ -17,8 +17,9 @@ const int ERROR 					= 1;
 void read_namespces_and_enter_them();
 // __attribute__((constructor))：在main函数之前执行某个函数
 // https://stackoverflow.com/questions/25704661/calling-setns-from-go-returns-einval-for-mnt-namespace
+// https://lists.linux-foundation.org/pipermail/containers/2013-January/031565.html
+
 __attribute__((constructor)) void init(void) {
-	printf("%s __attribute__((constructor)) void init(void) started\n", LOG_PREFIX);
 	read_namespces_and_enter_them();
 }
 
