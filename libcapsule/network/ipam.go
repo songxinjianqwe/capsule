ec2426ea88f616ecf8ae4a72fee87414bba0f2cf
songxinjianqwe <songxinjianzx@163.com>
2019-03-10 13:30:59 +0800

    添加容器网络

--- libcapsule/network/ipam.go
+++ libcapsule/network/ipam.go
@@ -1,80 +1,29 @@
 package network
 
 import (
-	"encoding/json"
 	"github.com/songxinjianqwe/capsule/libcapsule"
-	"io/ioutil"
 	"net"
-	"os"
-	"path"
 	"sync"
 )
 
 const IPAMDefaultAllocatorPath = libcapsule.RuntimeRoot + "/network/ipam/subnet.json"
 
-var once sync.Once
-var singletonIPAM *DefaultIPAM
-var singletonErr error
-
-func LoadIPAllocator() (IPAM, error) {
-	once.Do(func() {
-		singletonIPAM = &DefaultIPAM{
-			subnetAllocatorPath: IPAMDefaultAllocatorPath,
-		}
-		// load
-		if _, err := os.Stat(singletonIPAM.subnetAllocatorPath); err != nil && !os.IsNotExist(err) {
-			singletonErr = err
-		}
-		bytes, err := ioutil.ReadFile(singletonIPAM.subnetAllocatorPath)
-		if err != nil {
-			singletonErr = err
-		}
-		if err := json.Unmarshal(bytes, &singletonIPAM.subnetMap); err != nil {
-			singletonErr = err
-		}
-	})
-	return singletonIPAM, singletonErr
-}
-
+// ipam is short for ip address management
 type IPAM interface {
 	Allocate(subnet *net.IPNet) (net.IP, error)
 	Release(subnet *net.IPNet, ip *net.IP) error
 }
 
-// ipam is short for ip address management
-type DefaultIPAM struct {
-	subnetAllocatorPath string
-	subnetMap           map[string]string
-}
+var once sync.Once
+var singletonIPAM *DefaultIPAMImpl
+var singletonErr error
 
-func (ipam *DefaultIPAM) Allocate(subnet *net.IPNet) (net.IP, error) {
-	panic("implement me")
-}
-
-func (ipam *DefaultIPAM) Release(subnet *net.IPNet, ip *net.IP) error {
-	panic("implement me")
-}
-
-func (ipam *DefaultIPAM) dump() error {
-	if _, err := os.Stat(ipam.subnetAllocatorPath); err != nil && os.IsNotExist(err) {
-		// 如果文件之前不存在，则先创建目录，再创建文件
-		// 否则覆盖原来的文件
-		dir := path.Dir(ipam.subnetAllocatorPath)
-		if err := os.MkdirAll(dir, 0644); err != nil {
-			return err
+func LoadIPAllocator() (IPAM, error) {
+	once.Do(func() {
+		singletonIPAM = &DefaultIPAMImpl{
+			subnetAllocatorPath: IPAMDefaultAllocatorPath,
 		}
-	}
-	subnetFile, err := os.OpenFile(ipam.subnetAllocatorPath, os.O_TRUNC|os.O_WRONLY|os.O_CREATE, 0644)
-	if err != nil {
-		return err
-	}
-	defer subnetFile.Close()
-	bytes, err := json.Marshal(ipam.subnetMap)
-	if err != nil {
-		return err
-	}
-	if _, err := subnetFile.Write(bytes); err != nil {
-		return err
-	}
-	return nil
+		singletonErr = singletonIPAM.load()
+	})
+	return singletonIPAM, singletonErr
 }
---
+++ libcapsule/network/ipam_impl.go
@@ -0,0 +1,61 @@
+package network
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net"
+	"os"
+	"path"
+)
+
+type DefaultIPAMImpl struct {
+	subnetAllocatorPath string
+	subnetMap           map[string]string
+}
+
+func (ipam *DefaultIPAMImpl) Allocate(subnet *net.IPNet) (net.IP, error) {
+	panic("implement me")
+}
+
+func (ipam *DefaultIPAMImpl) Release(subnet *net.IPNet, ip *net.IP) error {
+	panic("implement me")
+}
+
+func (ipam *DefaultIPAMImpl) load() error {
+	// load
+	if _, err := os.Stat(singletonIPAM.subnetAllocatorPath); err != nil && !os.IsNotExist(err) {
+		return err
+	}
+	bytes, err := ioutil.ReadFile(singletonIPAM.subnetAllocatorPath)
+	if err != nil {
+		return err
+	}
+	if err := json.Unmarshal(bytes, &ipam.subnetMap); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (ipam *DefaultIPAMImpl) dump() error {
+	if _, err := os.Stat(ipam.subnetAllocatorPath); err != nil && os.IsNotExist(err) {
+		// 如果文件之前不存在，则先创建目录，再创建文件
+		// 否则覆盖原来的文件
+		dir := path.Dir(ipam.subnetAllocatorPath)
+		if err := os.MkdirAll(dir, 0644); err != nil {
+			return err
+		}
+	}
+	subnetFile, err := os.OpenFile(ipam.subnetAllocatorPath, os.O_TRUNC|os.O_WRONLY|os.O_CREATE, 0644)
+	if err != nil {
+		return err
+	}
+	defer subnetFile.Close()
+	bytes, err := json.Marshal(ipam.subnetMap)
+	if err != nil {
+		return err
+	}
+	if _, err := subnetFile.Write(bytes); err != nil {
+		return err
+	}
+	return nil
+}
