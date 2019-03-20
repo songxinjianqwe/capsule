6cf9293f5f16db334b1e575e7c79171d73295ff8
songxinjianqwe <songxinjianzx@163.com>
2019-03-20 09:39:21 +0800

    去除内存型ipallocator的单测中读文件判断是否正确存储的操作

--- libcapsule/network/ipam_local_impl_test.go
+++ libcapsule/network/ipam_local_impl_test.go
@@ -1,11 +1,7 @@
 package network
 
 import (
-	"encoding/json"
-	"github.com/songxinjianqwe/capsule/libcapsule/constant"
 	"github.com/stretchr/testify/assert"
-	"github.com/willf/bitset"
-	"io/ioutil"
 	"net"
 	"testing"
 )
@@ -21,17 +17,6 @@ func TestLocalIPAM_Allocate_Release(t *testing.T) {
 	t.Logf("allocated IP: %s", ip)
 	defer allocator.Release(subnet, ip)
 	assert.Equal(t, uint(originalAllocatable-1), allocator.Allocatable(subnet))
-
-	bytes, err := ioutil.ReadFile(constant.IPAMDefaultAllocatorPath)
-	assert.Nil(t, err)
-	subnetMap := make(map[string]*bitset.BitSet)
-	err = json.Unmarshal(bytes, &subnetMap)
-	assert.Nil(t, err)
-	_, exist := subnetMap[subnet.String()]
-	assert.True(t, exist)
-	bitmap := subnetMap[subnet.String()]
-	// bitmap中一定有1
-	assert.True(t, bitmap.Any())
 
 	err = allocator.Release(subnet, ip)
 	assert.Nil(t, err)
