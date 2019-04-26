ce63b5c35fda77f5faa0624a44e0126af78a8cbf
songxinjianqwe <songxinjianzx@163.com>
2019-04-26 14:53:58 +0800

    代码优化

--- libcapsule/cgroups/sub_system_wrapper.go
+++ libcapsule/cgroups/sub_system_wrapper.go
@@ -46,7 +46,8 @@ func (subsys *SubsystemWrapper) Join(cgroupName string, pid int) (string, error)
 	// write pid
 	// tasks文件一般情况下cgroup控制无效，会在init process执行syscall.Exec后tasks文件被清空，暂不清楚原因
 	// cgroup.procs一定有效
-	// 您好，我在阅读runC的代码时发现cgroup manager#apply时有时间会将PID写入到tasks文件，有时候会写入到cgroup.procs文件中，于是我尝试将代码改为写入到cgroup.procs中，问题就解决了。
+	// runC的代码中cgroup manager#apply时有时候会将PID写入到tasks文件，有时候会写入到cgroup.procs文件中
+	// 将代码改为写入到cgroup.procs中，问题解决。
 	//
 	// tasks: list of tasks (by PID) attached to that cgroup. This list
 	// 		  is not guaranteed to be sorted. Writing a thread ID into this file
